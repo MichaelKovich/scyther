a87fe6b350b20e6e8a458325e4ab6fad3e632db8
Michael Kovich <kovich@michaelkovich.com>
2019-01-06 18:54:25 -0500

    Server information.

---
+++ commands/server.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'server',
+  description: 'Provides the user with information about the server.',
+  execute(message, args) {
+    message.channel.send(`This server's name is: ${message.guild.name}`);
+    message.channel.send(`There are ${message.guild.memberCount} members here.`);
+    message.channel.send(`${message.guild.name} was created on ${message.guild.createdAt}`);
+  },
+};
