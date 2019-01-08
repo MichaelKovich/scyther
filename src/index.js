398e0237e34e5d3e7786e2c5dc2f3f987255e718
Michael Kovich <kovich@michaelkovich.com>
2019-01-07 19:01:05 -0500

    Minor reorganization.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 
-const {prefix, token} = require('./config/config.json');
+const {prefix, token} = require(`${__dirname}/../config/config.json`);
+const {greetings} = require(`${__dirname}/utils/constants`);
+
+const commandFiles = fs.readdirSync(`${__dirname}/commands`).filter(file => file.endsWith('.js'));
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const greetingGenerator = (member) => {
+  const index = Math.floor(Math.random() * greetings.length);
+  return greetings[index].replace(/member/, member);
+};
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const command = require(`${__dirname}/commands/${file}`);
   client.commands.set(command.name, command);
 }
 
@@ -28,6 +34,13 @@ client.on('message', (message) => {
     console.log('An Error Has Occurred: ', error);
     message.reply('I encountered an error trying to execute that command.');
   }
+});
+
+client.on('guildMemberAdd', (member) => {
+  const channels = ['general-text', 'lumbridge'];
+  const channel = member.guild.channels.find(ch => channels.includes(ch.name));
+  if (!channel) return;
+  channel.send(greetingGenerator(member));
 });
 
 client.login(token);
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  greetings: [
+    '*uses scratch against member.* It was super effective!',
+    '*offers member a dead bird.* Delicious!',
+    '*stares closely at member*',
+    '*pounces and snatches a french fry from the hands of member*',
+  ],
+};
